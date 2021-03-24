c7dcaec5f6347561032538fcdb1d3e20a7bf63f6
Ronaldo <ronaldofigueiredosp@gmail.com>
2021-03-24 14:21:32 -0300

    adding new components on app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { Header } from './components/Header'
+import { Dashboard } from './components/Dashboard'
+
 import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
-    <div className="App">
+    <>
       <GlobalStyle />
-      <h1>Ae Carai</h1>
-    </div>
+      <Header />
+      <Dashboard />
+    </>
   );
 }
